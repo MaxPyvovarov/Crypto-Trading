89f4379dca457d6d2204c106a11030b9efbfc5b1
MaxPyvovarov <max.pivovar@gmail.com>
2022-10-31 19:20:32 +0200

    Technologies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from './App.module.scss';
 
 import {Navbar, Home, Chart} from './components';
-import {banner} from './assets';
+import {banner, circles} from './assets';
 
 const App = () => (
 	<div className={styles.wrapper}>
@@ -10,6 +10,7 @@ const App = () => (
 			<img className={styles.banner} src={banner} alt='banner' />
 			<Home />
 			<Chart />
+			<img src={circles} alt='background' className={styles.circles} />
 		</div>
 	</div>
 );
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 import styles from './Chart.module.scss';
+import {chart} from '../../assets';
 
-const Chart = () => {
-	return <div>Chart</div>;
-};
+const Chart = () => (
+	<section id='technology' className={styles.technology}>
+		<div className={styles.heading}>
+			<h3>About us</h3>
+			<h1>Global technologies</h1>
+		</div>
+		<div className={styles.content}>
+			<div className={styles.chart}>
+				<img src={chart} alt='chart' />
+			</div>
+			<div className={styles.caption}>
+				<div className={styles.caption__level}>
+					<h4>Level 1</h4>
+					<p>
+						Deploy a multi-legged futures strategy by simultaneously placing
+						orders to buy and sell from the same order ticket.
+					</p>
+					<p>
+						Place and manage orders based on the Level 2 order book, which
+						provides a live display of all bids and offers waiting to be
+						executed.
+					</p>
+				</div>
+				<button className={styles.btn}>Start now</button>
+			</div>
+		</div>
+	</section>
+);
 
 export default Chart;
